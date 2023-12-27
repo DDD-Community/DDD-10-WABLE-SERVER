7b8b59c431e705e4622523e303252f800f66b5d7
Minsoo-Shin <ms.shin@acloset.app>
2023-12-27 23:59:04 +0900

    init project

---
+++ src/main/java/com/wable/harmonika/HarmonikaApplication.java
@@ -0,0 +1,13 @@
+package com.wable.harmonika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wable/harmonika/HarmonikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wable.harmonika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
