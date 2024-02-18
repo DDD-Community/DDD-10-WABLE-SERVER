06f3f01bb58480e5c76bbccac4c29a2029e02af5
aihoshistar <troublesome.dev@gmail.com>
2024-02-19 01:45:52 +0900

    jasypt 추가

---
+++ src/test/java/com/wable/harmonika/domain/card/repository/JasyptConfigAESTest.java
@@ -0,0 +1,29 @@
+package com.wable.harmonika.domain.card.repository;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.junit.jupiter.api.Test;
+
+class JasyptConfigAESTest {
+
+    @Test
+    void stringEncryptor() {
+        String url = "jdbc:mysql://localhost:3306/default";
+        String username = "root";
+        String password = "password";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "dddTest";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        pbeEnc.setPassword(key);
+        pbeEnc.setIvGenerator(new RandomIvGenerator());
+        return pbeEnc.encrypt(value);
+    }
+}
