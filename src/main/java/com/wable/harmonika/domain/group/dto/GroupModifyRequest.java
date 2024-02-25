c64d7e449ec9b6055f7a428197c7df3f360a568d
aihoshistar <troublesome.dev@gmail.com>
2024-02-26 01:57:29 +0900

    [REFACTOR/GROUP]: 그룹내 팀원 역할 수정 할 수 있도록 함

--- src/main/java/com/wable/harmonika/domain/group/dto/GroupModifyRequest.java
+++ src/main/java/com/wable/harmonika/domain/group/dto/GroupModifyRequest.java
@@ -1,11 +1,14 @@
 package com.wable.harmonika.domain.group.dto;
 
 import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class GroupModifyRequest {
 
+    @NotBlank
     private String name;
 
     private List<FixedQuestion> fixedQuestions = List.of();
