11047aab0d95ea9b56f07f795c9bee45451f7d84
brandon-shin <64356885+Minsoo-Shin@users.noreply.github.com>
2024-03-01 13:44:06 +0900

    [feat/cors] cors 허용 (#37)

--- src/main/java/com/wable/harmonika/global/config/WebConfig.java
+++ src/main/java/com/wable/harmonika/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.wable.harmonika.global.config;
 import com.wable.harmonika.global.auth.UserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,5 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new UserArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
