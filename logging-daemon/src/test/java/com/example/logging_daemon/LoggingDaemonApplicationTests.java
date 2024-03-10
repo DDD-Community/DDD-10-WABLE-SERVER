4b019cb3b3fe5d7737148afdc6fd8a8491b830b7
aihoshistar <troublesome.dev@gmail.com>
2024-03-11 01:38:44 +0900

    [REFACTOR/MONO_REPO]: Loggin 서버에 대한 프로젝트 추가

---
+++ logging-daemon/src/test/java/com/example/logging_daemon/LoggingDaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logging_daemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingDaemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
