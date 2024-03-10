f9f5f9cafe2b00475c4ffd3803cab3f514f72e93
aihoshistar <troublesome.dev@gmail.com>
2024-03-11 01:38:57 +0900

    [REFACTOR/MONO_REPO]: 데이터 통계 배치 서버에 대한 프로젝트 추가

---
+++ statistics-daemon/src/main/java/com/wable/statistics_daemon/StatisticsDaemonApplication.java
@@ -0,0 +1,13 @@
+package com.wable.statistics_daemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsDaemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsDaemonApplication.class, args);
+	}
+
+}
---
+++ statistics-daemon/src/test/java/com/wable/statistics_daemon/StatisticsDaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wable.statistics_daemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsDaemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
