24502629985e0c4a60c160596141e1a17d93d1be
aihoshistar <troublesome.dev@gmail.com>
2024-03-15 01:22:09 +0900

    BACKEND-3: 프로젝트 init

---
+++ event-api/src/main/java/com/wable/event_api/EventApiApplication.java
@@ -0,0 +1,13 @@
+package com.wable.event_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventApiApplication.class, args);
+	}
+
+}
---
+++ event-api/src/test/java/com/wable/event_api/EventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wable.event_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
