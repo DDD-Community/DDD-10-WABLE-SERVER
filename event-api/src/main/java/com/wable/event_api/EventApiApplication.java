4abf6eafc5e55a9831dfc81bdf772d4cc96243a3
aihoshistar <troublesome.dev@gmail.com>
2024-03-21 23:03:47 +0900

    BACKEND-3: add spring webflux

--- event-api/src/main/java/com/wable/event_api/EventApiApplication.java
+++ event-api/src/main/java/com/wable/event_api/EventApiApplication.java
@@ -1,13 +1,18 @@
 package com.wable.event_api;
 
+import com.wable.event_api.greeting.GreetingClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
 public class EventApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventApiApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EventApiApplication.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		// We need to block for the content here or the JVM might exit before the message is logged
+		System.out.println(">> message = " + greetingClient.getMessage().block());
 	}
-
 }
